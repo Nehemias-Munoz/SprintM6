14cdc3cbc431eb8bfe4296b80dc3408f9c407d35
Nehemias Munoz <nehemiasmunozm@gmail.com>
2023-07-10 19:24:06 -0400

    Fix: Mezcla datos en clase Inst.Salud

--- src/main/java/com/marvic/springm6/entity/InstitucionPrevision.java
+++ src/main/java/com/marvic/springm6/entity/InstitucionPrevision.java
@@ -5,28 +5,14 @@ import lombok.Data;
 
 @Data
 @Entity
-@Table(name = "Trabajador")
+@Table(name = "Institucion_Prevision")
 public class InstitucionPrevision {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id_trabajador")
-    private int idTrabajador;
+    @Column(name = "id_inst_prevision")
+    private int idInstPrevision;
     @Column
-    private int run;
-    @Column
-    private String nombre;
-    @Column(name = "apellido_1")
-    private String apellido1;
-    @Column(name = "apellido_2")
-    private String apellido2;
-    @Column
-    private String email;
-    //Todo: Crear relacion y FK
-    @Column(name = "inst_prevision")
-    private int instPrevision;
-    //Todo: Crear relacion y FK
-    @Column(name = "inst_salud")
-    private int instSalud;
-    @Column
-    private long telefono;
+    private String descripcion;
+    @Column(name = "porc_dcto")
+    private float porcDcto;
 }
