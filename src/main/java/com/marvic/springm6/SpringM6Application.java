79660f9191afabe8cf17efc978756c41a9579144
Nehemias Munoz <nehemiasmunozm@gmail.com>
2023-07-10 10:20:23 -0400

    Inicial

---
+++ src/main/java/com/marvic/springm6/SpringM6Application.java
@@ -0,0 +1,13 @@
+package com.marvic.springm6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringM6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringM6Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/marvic/springm6/SpringM6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marvic.springm6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringM6ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
