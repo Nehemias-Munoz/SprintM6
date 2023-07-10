3732267bdf7c446d31cececc569bce1532028417
Nehemias Munoz <nehemiasmunozm@gmail.com>
2023-07-10 19:22:35 -0400

    Add: clase institucion salud

---
+++ src/main/java/com/marvic/springm6/entity/InstitucionSalud.java
@@ -0,0 +1,19 @@
+package com.marvic.springm6.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.springframework.context.annotation.Primary;
+
+@Data
+@Entity
+@Table(name = "Institucion_Salud")
+public class InstitucionSalud {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_inst_salud")
+    private int idInstSalud;
+    @Column
+    private String descripcion;
+    @Column(name = "porc_dcto")
+    private float porcDcto;
+}
