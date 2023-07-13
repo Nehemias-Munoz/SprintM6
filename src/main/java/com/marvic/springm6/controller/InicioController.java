ca89e25e570b94c1e140c1e9c495168cb33d0dda
Nehemias Munoz <nehemiasmunozm@gmail.com>
2023-07-12 21:55:07 -0400

    Add: controller de inicio

--- src/main/java/com/marvic/springm6/controller/InicioController.java
+++ src/main/java/com/marvic/springm6/controller/InicioController.java
@@ -1,10 +1,20 @@
 package com.marvic.springm6.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class InicioController {
     @GetMapping("/")
     public String index(){
         return "index";
+    }
+
+    @GetMapping("/inicioSesion")
+    public String inicioSesion(){return "login";}
+
+    @GetMapping("/registro")
+    public String registro(){
+        return "registro";
     }
 }
